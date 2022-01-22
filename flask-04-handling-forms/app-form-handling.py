0ab6bc2b63f9339aaaf969123b9f810e0ca64908
EC2 Default User <ec2-user@ip-172-31-86-63.ec2.internal>
2022-01-22 08:15:20 +0000

    flask-4 added

---
+++ flask-04-handling-forms/app-form-handling.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template ("main.html",name = "Elif")
+
+@app.route("/greet", methods = ["GET"])
+def greet():
+    if 'user' in request.args:
+        usr = request.args['user']
+        return render_template("greet.html", user=usr)
+    else:
+        return render_template("greet.html", user="Send your user name with 'user' parameter in ? ")
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    if request.method == 'POST':
+        user_name = request.form['username']
+        password = request.form['password']
+        if password == 'clarusway':
+            return render_template("secure.html", user = user_name.title())
+        else:
+            return render_template("login.html", user = user_name.title(), control = True)
+    else:
+        return render_template("login.html", control = False)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
